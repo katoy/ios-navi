81e5f979159587627928af181b89615ef619a149
katoy <youichikato@gmail.com>
2014-05-05 08:46:21 +0900

    Initial Commit

---
+++ navi/NaviAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NaviAppDelegate.h
+//  navi
+//
+//  Created by katoy on 2014/05/05.
+//  Copyright (c) 2014年 Youichi Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NaviAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ navi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  navi
+//
+//  Created by katoy on 2014/05/05.
+//  Copyright (c) 2014年 Youichi Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NaviAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NaviAppDelegate class]));
+    }
+}
